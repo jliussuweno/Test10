0346e1c5a8812afb9f6338a6efdf6d7e81eed29a
Jlius Suweno <jliuss@live.com>
2020-12-08 16:25:01 +0700

    update

--- Test10/ViewController.m
+++ Test10/ViewController.m
@@ -6,7 +6,6 @@
 //
 
 #import "ViewController.h"
-#import <RegisterTemplateIOS13/RegisterViewController.h>
 
 @interface ViewController ()
 
@@ -17,23 +16,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-}
-
-- (void)viewDidAppear:(BOOL)animated {
-    
-    double delayInSeconds = 1.0;
-    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-        
-        NSBundle *podBundle = [NSBundle bundleForClass:[RegisterViewController class]];
-        id data = [podBundle URLForResource:@"RegisterTemplateIOS13" withExtension:@"bundle"];
-        NSBundle *bundle = [NSBundle bundleWithURL:data];
-        RegisterViewController *loginView = [[RegisterViewController alloc]initWithNibName:@"RegisterViewController" bundle:bundle];
-        loginView.modalPresentationStyle = UIModalPresentationFullScreen;
-        [self presentViewController:loginView animated:YES completion:nil];
-        
-        
-    });
 }
 
 
